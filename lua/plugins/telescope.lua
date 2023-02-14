31d48dd5b51718eab0e1062451cb1ad412993a53
Curt Bushko <cbushko@gmail.com>
2023-02-13 23:59:39 -0500

    fill out keymaps for find files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,95 @@ return {
       -- stylua: ignore
       {
         "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
         desc = "Find Plugin File",
+      },
+
+      {
+        "<leader>fb",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "﬘ Find buffers",
+      },
+      {
+        "<leader>fc",
+        function()
+          require("telescope.builtin").grep_string()
+        end,
+        desc = " Find text under cursor",
+      },
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files({ cwd = "." })
+        end,
+        desc = " Find files",
+      },
+      {
+        "<leader>fF",
+        function()
+          require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
+        end,
+        desc = " Find all files",
+      },
+      {
+        "<leader>fh",
+        function()
+          require("telescope.builtin").help_tags()
+        end,
+        desc = "ﬤ Help",
+      },
+      {
+        "<leader>fk",
+        function()
+          require("telescope.builtin").keymaps()
+        end,
+        desc = "  Keymaps",
+      },
+      {
+        "<leader>fm",
+        function()
+          require("telescope.builtin").marks()
+        end,
+        desc = " Marks",
+      },
+      {
+        "<leader>fn",
+        "<CMD>enew<CR>",
+        desc = " New File",
+      },
+      {
+        "<leader>fr",
+        function()
+          require("telescope.builtin").oldfiles()
+        end,
+        desc = " Recent",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = " Grep",
+      },
+      {
+        "<leader>ft",
+        ":TodoTelescope<CR>",
+        desc = " Todo",
+      },
+      {
+        "<leader>fG",
+        function()
+          require("telescope.builtin").live_grep({
+            additional_args = function(args)
+              return vim.list_extend(args, { "--hidden", "--no-ignore" })
+            end,
+          })
+        end,
+        desc = " Grep all files (hidden)",
       },
     },
     -- change some options
