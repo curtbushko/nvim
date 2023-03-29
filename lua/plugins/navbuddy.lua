08415a92abb5893bfb67b88fcfcd636a86d49ef6
Curt Bushko <cbushko@gmail.com>
2023-03-29 11:31:53 -0400

    add nvim-navbuddy, remove aerial

---
+++ lua/plugins/navbuddy.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "SmiteshP/nvim-navbuddy",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "SmiteshP/nvim-navic",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      local navbuddy = require("nvim-navbuddy")
+      navbuddy.setup({
+        window = {
+          border = "none",
+        },
+        lsp = { auto_attach = true, preference = "nvim_lsp" },
+      })
+    end,
+
+    keys = {
+      { "<leader>n", "<CMD>Navbuddy<CR>", desc = " Navbuddy" },
+    },
+  },
+}
