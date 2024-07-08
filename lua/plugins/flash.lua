5458b56d7a4f544c59c6f919094f582b1454d58b
Curt Bushko <cbushko@gmail.com>
2024-07-08 10:43:22 -0400

    flash.lua: work around breaking change where flash + search were broken

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,6 +1,16 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
+  opts = {
+    modes = {
+      char = {
+        jump_labels = true,
+      },
+      search = {
+        enabled = true,
+      },
+    },
+  },
   keys = {
     { "s", mode = { "n", "x", "o" }, false },
     { "<leader>/", false },
